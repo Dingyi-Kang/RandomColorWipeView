94567f30e82ddbf1e818196d13b01e886b64c405
Dingyi <dkang@okstate.edu>
2021-04-16 16:47:50 -0500

    Origin

--- 50Cards/ContentView.swift
+++ 50Cards/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    var color:Color{
+        return Color(
+            .sRGB,
+            red: Double.random(in: 0..<1),
+            green: Double.random(in: 0..<1),
+            blue: Double.random(in: 0..<1),
+            opacity: 1
+        )
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        TabView(){
+            
+            ForEach (1...10, id: \.self){ p in
+                GeometryReader{ geo in
+                    ZStack{
+                        Rectangle()
+                            .foregroundColor(color)
+                            .cornerRadius(10.0)
+                            .shadow(radius: 30)
+                        Image(systemName: "\(p).circle")
+                            .resizable()
+                            .scaledToFit()
+                            .padding()
+                    }.frame(height: max(geo.size.height-50, 100))
+                    
+                    
+                }
+                
+                
+            }
             .padding()
+            
+        }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 
