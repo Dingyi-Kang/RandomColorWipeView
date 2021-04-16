6fe8d96c6ca8dc6b88711ce21e99a2c27938c548
Dingyi <dkang@okstate.edu>
2021-04-16 15:37:27 -0500

    Initial Commit

---
+++ 50Cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  50Cards
+//
+//  Created by 定一康 on 4/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
